61c0e73a81de82d0887ccef20267a0cf6c019e84
Audrey Huang <ottery@Audreys-MacBook-Pro.local>
2016-09-06 23:52:57 -0700

    cdhit analysis

--- cdhit_analysis.py
+++ cdhit_analysis.py
@@ -24,26 +24,20 @@ def convert_to_fasta(inp):
 
 
 def cluster(path, thresh, inp): 
-	if thresh >= 0.6: 
-		command = path + 'cd-hit -i ' + inp + '.sequences -o ' + inp + str(60) + '.sequences -c ' + \
-		str(thresh) + ' -n 4 -M 16000 -d 0 -T 8'
 
-		status = subprocess.call(command, shell=True)
+	command = path + 'cd-hit -i ' + inp + '.faa -o ' + inp + str(thresh * 100) + ' -c ' + \
+	str(thresh) + ' -n 5 -M 16000 -d 0 -T 8'
 
-	elif thresh >= 0.8: 
-		command = path + 'cd-hit -i ' + inp + '.sequences -o ' + inp + str(thresh * 100) + '.sequences -c ' + \
-		str(thresh * 100) + ' -n 5 -M 16000 -d 0 -T 8'
-
-		print(command)
-		# status = subprocess.call(command, shell=True)
+	print(command)
+	status = subprocess.call(command, shell=True)
 
 
 def main(): 
 	cdhit = '../tools/cdhit/'
 	thresh = 0.9
 	inp = '../proteins_that_were_hit'
-	# cluster(cdhit, thresh, inp)
-	convert_to_fasta(inp)
+	cluster(cdhit, thresh, inp)
+	# convert_to_fasta(inp)
 
 
 if __name__ == '__main__':
