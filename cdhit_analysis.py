7043e7f9a33b03bfd5f0bf979eaa73ab1d893aaf
Audrey Huang <ottery@Audreys-MacBook-Pro.local>
2016-09-07 11:34:57 -0700

    superfamily 80 cdhit

--- cdhit_analysis.py
+++ cdhit_analysis.py
@@ -23,6 +23,15 @@ def convert_to_fasta(inp):
 					out.write(fa[1].strip() + '\n')
 
 
+def concat_fasta(file1, file2, out): 
+	with open(out, 'w') as out: 
+		with open(file1, 'r') as f1: 
+			for line in f1: 
+				out.write(line) 
+		with open(file2, 'r') as f2: 
+			for line in f2: 
+				out.write(line)
+
 
 def cluster(path, thresh, inp): 
 
@@ -35,11 +44,13 @@ def cluster(path, thresh, inp):
 
 def main(): 
 	cdhit = '../tools/cdhit/'
-	thresh = 0.4
+	thresh = 0.8
 	inp = '../proteins_that_were_hit'
+	out = '../hits_and_superfamily.faa'
 
-	convert_to_fasta(inp)
-	cluster(cdhit, thresh, inp)
+	# convert_to_fasta(inp)
+	concat_fasta(out, inp + '.faa', 'sfld_superfamily_122.fasta')
+	cluster(cdhit, thresh, out)
 	
 
 
