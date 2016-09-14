0f5a7c04a3531cd4dc6b29f08a2d0b6f9cb7dbdf
audhuang <audreynhuang@gmail.com>
2016-09-14 12:47:35 -0700

    create heatmap

--- create_heatmap.py
+++ create_heatmap.py
@@ -75,6 +75,7 @@ def projectimg_dic(project_path, pickle_path):
 def projecthit_dic(cluster_path): 
 	project_hit_dic = {}
 	id_cluster_dic = {}
+	project_cluster_dic = {}
 
 	with open(cluster_path, 'r') as f: 
 		cluster = -1
@@ -94,12 +95,20 @@ def projecthit_dic(cluster_path):
 				else: 
 					id_cluster_dic[name_cluster].append(cluster) 
 
+				if name not in project_cluster_dic: 
+					project_cluster_dic[name] = [cluster]
+				else: 
+					project_cluster_dic[name].append(cluster)
+
+
 	with open(r'project_hit_dic.pickle', 'wb') as out: 
 		cp.dump(project_hit_dic, out)
 	with open(r'id_cluster_dic.pickle', 'wb') as out: 
 		cp.dump(id_cluster_dic, out)
+	with open(r'project_cluster_dic.pickle', 'wb') as out: 
+		cp.dump(project_cluster_dic, out)
 
-	return project_hit_dic, id_cluster_dic
+	# return project_hit_dic, id_cluster_dic, project_cluster_dic
 
 
 def get_subgroups(table_path): 
@@ -120,7 +129,7 @@ def get_subgroups(table_path):
 	return subgroups
 
 
-def parse_table(table_path, subgroups): 
+def parse_table(table_path): 
 	dic = {}
 	cluster_dic = {}
 
@@ -130,20 +139,18 @@ def parse_table(table_path, subgroups):
 		subgroups = cp.load(inp)
 
 	with open(table_path, 'r') as f: 
-		for i in range(3): 
-			f.next()
-		for line in f: 
-			col = line.split()
-			subgroup = col[2]
-			name = col[0].split('|')[0]
-			cluster = id_cluster_dic[name]
+		if line[0] != '#': 
+			for line in f: 
+				col = line.split()
+				subgroup = col[2]
+				name = col[0].split('|')[0]
 
-			if name not in dic: 
-				dic[name] = [[]] * len(subgroups)
+				if name not in dic: 
+					dic[name] = [[]] * len(subgroups)
 				dic[name][subgroups.index[subgroup]].append(id_cluster_dic[col[0]])
 
 	for key in dic: 
-		cluster_dic[key] = len(list(set(dic[key])))
+		cluster_dic[key][:] = [len(set(dic[key][i])) for i in len(subgroups)]
 
 	with open(r'cluster_ids.pickle', 'wb') as out: 
 		cp.dump(dic, out)
@@ -152,6 +159,90 @@ def parse_table(table_path, subgroups):
 
 	return cluster_dic
 
+
+# hits or unique clusters?
+def choose_surveys(num): 
+	with open(r'project_img_dic.pickle', 'rb') as inp: 
+		project_img_dic = cp.load(inp)
+	# with open(r'project_hit_dic.pickle', 'rb') as inp: 
+	# 	project_hit_dic = cp.load(inp)
+	with open(r'project_cluster_dic.pickle', 'rb') as inp: 
+		project_cluster_dic = cp.load(inp)
+
+
+	biggest = []
+	for key in project_img_dic: 
+		biggest = 0
+		biggest_index = 0
+		imgs = project_img_dic[key]
+
+		for i in range(len(imgs)): 
+			num = len(set(project_cluster_dic[imgs[i]]))
+			if num > biggest: 
+				biggest = num
+				biggest_index = i
+
+		biggest.append((key, biggest, biggest_index))
+
+	sorted_biggest = sorted(biggest, key=lambda tup: tup[1])
+	projects = [x[0] for x in sorted_biggest][:num]
+
+
+	# with open(r'most_hits_dic.pickle', 'wb') as out: 
+	# 	cp.dump(biggest_dic, out)
+	with open(r'biggest_list.pickle', 'wb') as out: 
+		cp.dump(biggest, out)
+	with open(r'sorted_biggest_list.pickle', 'wb') as out: 
+		cp.dump(sorted_biggest, out)
+
+	return projects
+
+
+def write_rfile(projects, rout_path): 
+	with open(r'cluster_counts.pickle', 'rb') as inp: 
+		cluster_dic = cp.load(inp)
+	with open(r'subgroups.pickle', 'rb') as inp: 
+		subgroups = cp.load(inp)
+
+	with open(rout_path, 'w') as f: 
+		write = csv.writer(f, delimiter=',')
+		write.writerow([''] + subgroups)
+
+		for project in projects: 
+			write.writerow([project] + cluster_dic[project])
+
+
+def get_colors(projects): 
+	with open(r'img_project_dic.pickle', 'rb') as inp: 
+		img_project_dic = cp.load(inp)
+	color_dic = {}
+
+	for project in projects: 
+		if isfile('../config/' + project + '.config'): 
+			with open('../config/' + project + '.config', 'r') as f: 
+				for line in f: 
+				linelist = line.split(' ')
+					if 'phylum' in linelist[0]: 
+						color_dic[project] = linelist[1].strip().lower()
+		elif isfile('../new_config/' + project + '.config'): 
+			with open('../new_config/' + project + '.config', 'r') as f: 
+				for line in f: 
+				linelist = line.split(' ')
+					if 'phylum' in linelist[0]: 
+						color_dic[project] = linelist[1].strip().lower()
+		else: 
+			with open('../no_phylum.txt', 'a') as f: 
+				write = csv.writer(f, delimiter='\t')
+				write.writerow(img_project_dic[project])
+
+
+	with open(r'color_dic.pickle', 'wb') as out: 
+		cp.dump(color_dic, out)
+
+
+def generate_heatmap(): 
+	command = 'R CMD BATCH heatmap_all.R'
+	status = subprocess.call(command, shell=True)
 
 
 				
@@ -165,13 +256,22 @@ def main():
 	# table_path = '../example.table'
 	table_path = '../results.table'
 
+	rout_path = './all_data'
+
 	# cut_length(seq_path, fasta_cut_path, 50, 1000)
 	# cluster(cdhit_path, fasta_cut_path[:-4], 0.9, 5)
+
 	# project_img_dic, img_project_dic = projectimg_dic(project_path, pickle_path)
 	# project_hit_dic, id_cluster_dic = projecthit_dic(cluster_path)
+	projecthit_dic(cluster_path)
 	# subgroups = get_subgroups(table_path)
-	parse_table(table_path, subgroups)
 
+	parse_table(table_path, subgroups)
+	projects = choose_surveys(100)
+
+	write_rfile(projects, rout_path)
+	get_colors(projects)
+	# generate_heatmap()
 
 
 if __name__ == '__main__':
